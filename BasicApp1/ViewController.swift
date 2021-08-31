ad8357d6cd538b0995b5af7da3732c16ecdc29fa
YutaroNakaji <yutaro.nakaji@four-brains.com>
2021-08-31 18:00:06 +0900

    Initial Commit

---
+++ BasicApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicApp1
+//
+//  Created by Yutaro Nakaji（B） on 2021/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
